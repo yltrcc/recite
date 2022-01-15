87353fd084a0a97221219b2164e1b1ba69da0c41
yltrcc <ttxxly@gmail.com>
2022-01-15 08:30:07 +0800

    新增题库详情页，以及首页跳转功能。

--- app/src/main/java/com/yltrcc/app/recite/MainActivity.kt
+++ app/src/main/java/com/yltrcc/app/recite/MainActivity.kt
@@ -2,10 +2,22 @@ package com.yltrcc.app.recite
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.content.Intent
+import android.widget.LinearLayout
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val javaBase: LinearLayout = findViewById(R.id.ll_java_base)
+        javaBase.setOnClickListener {
+            val intent = Intent()
+            intent.setClass(this@MainActivity, QuestionDetailsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/yltrcc/app/recite/QuestionDetailsActivity.kt
@@ -0,0 +1,17 @@
+package com.yltrcc.app.recite
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class QuestionDetailsActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question_details)
+
+    }
+
+}
